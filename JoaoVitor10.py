70771942855fc78eaa7b92fba3fc75cd269101f9
João Vitor Muniz Vasconcelos <140927075+JoaoVitorMuniz16@users.noreply.github.com>
2023-10-18 11:11:49 -0300

    Add files via upload

---
+++ JoaoVitor10.py
@@ -0,0 +1,5 @@
+Velocidade = int(input("digite a velocidade em km/h"))
+if Velocidade > int("60"):
+    print("você tomou uma muta por excesso")
+else:
+    print("você está na velocidade permitida")
